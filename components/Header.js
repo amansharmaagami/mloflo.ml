6d2442bed9448499ad3a653882814c12ad501063
Aman Sharma <amans@agamitechnologies.com>
2022-07-07 00:10:36 +0530

    Import images

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Router from "next/router";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -8,6 +10,7 @@ export default function Header() {
 	function handleSubmit(e) {
 		e.preventDefault();
 		Router.push("/inbox/" + inbox);
+		setIsMenuOpen(false);
 	}
 
 	return (
@@ -16,14 +19,21 @@ export default function Header() {
 				<div className="mx-auto px-4 sm:px-6">
 					<div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
 						<div className="flex justify-start lg:w-0 lg:flex-1">
-							<a href="/">
-								<span className="sr-only">Workflow</span>
-								<img
-									className="h-8 w-auto sm:h-10"
-									src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-									alt=""
-								/>
-							</a>
+							<Link href="/">
+								<a>
+									<span className="sr-only">Workflow</span>
+									{/* <img className="h-8 w-auto sm:h-10" src="/logo.svg" /> */}
+									<div className="h-8 w-auto">
+										<Image
+											src="/logo.svg"
+											alt="mloflo.ml"
+											height={40}
+											width="100%"
+											layout="fixed"
+										/>
+									</div>
+								</a>
+							</Link>
 						</div>
 						<div className="-mr-2 -my-2 md:hidden">
 							<button
@@ -52,31 +62,31 @@ export default function Header() {
 							</button>
 						</div>
 						<nav className="hidden md:flex space-x-10">
-							<a
+							<Link
 								href="#"
 								className="text-base font-medium text-gray-500 hover:text-gray-900"
 							>
 								Solutions
-							</a>
-							<a
+							</Link>
+							<Link
 								href="#"
 								className="text-base font-medium text-gray-500 hover:text-gray-900"
 							>
 								Pricing
-							</a>
-							<a
+							</Link>
+							<Link
 								href="#"
 								className="text-base font-medium text-gray-500 hover:text-gray-900"
 							>
 								Docs
-							</a>
+							</Link>
 
-							<a
+							<Link
 								href="#"
 								className="text-base font-medium text-gray-500 hover:text-gray-900"
 							>
 								More
-							</a>
+							</Link>
 						</nav>
 						<div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
 							<form className="flex items-center" onSubmit={handleSubmit}>
@@ -106,11 +116,15 @@ export default function Header() {
 							<div className="pt-5 pb-6 px-5">
 								<div className="flex items-center justify-between">
 									<div>
-										<img
-											className="h-8 w-auto"
-											src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-											alt="Workflow"
-										/>
+										<div className="h-8 w-auto">
+											<Image
+												src="/logo.svg"
+												alt="mloglo.ml"
+												width={50}
+												height={50}
+												layout="intrinsic"
+											/>
+										</div>
 									</div>
 									<div className="-mr-2">
 										<button
@@ -141,49 +155,65 @@ export default function Header() {
 							</div>
 							<div className="py-6 px-5 space-y-6">
 								<div className="grid grid-cols-2 gap-y-4 gap-x-8">
-									<a
+									<Link
 										href="#"
 										className="text-base font-medium text-gray-900 hover:text-gray-700"
 									>
 										Solutions
-									</a>
+									</Link>
 
-									<a
+									<Link
 										href="#"
 										className="text-base font-medium text-gray-900 hover:text-gray-700"
 									>
 										Pricing
-									</a>
+									</Link>
 
-									<a
+									<Link
 										href="#"
 										className="text-base font-medium text-gray-900 hover:text-gray-700"
 									>
 										Docs
-									</a>
+									</Link>
 
-									<a
+									<Link
 										href="#"
 										className="text-base font-medium text-gray-900 hover:text-gray-700"
 									>
 										More
-									</a>
+									</Link>
 								</div>
 								<div>
-									<a
+									<Link
 										href="#"
 										className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
 									>
 										Sign up
-									</a>
+									</Link>
 									<p className="mt-6 text-center text-base font-medium text-gray-500">
-										Existing customer? &nbsp;
-										<a
+										{/* Existing customer? &nbsp;
+										<Link
 											href="#"
 											className="text-indigo-600 hover:text-indigo-500"
 										>
 											Sign in
-										</a>
+										</Link> */}
+										<form className="flex items-center" onSubmit={handleSubmit}>
+											<input
+												className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+												id="inbox"
+												type="text"
+												placeholder="view-this-inbox"
+												value={inbox}
+												onChange={(e) => setInbox(e.target.value)}
+											/>
+											<button
+												type="submit"
+												className="ml-2 whitespace-nowrap inline-flex items-center justify-center px-3 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+											>
+												View Inbox
+											</button>
+										</form>
 									</p>
 								</div>
 							</div>
